2eb6f5b03406fc0b36ecb6ef3b775ca34280460f
Sanket Patil <55378662+Sanketpatil45@users.noreply.github.com>
2020-10-01 16:35:53 +0530

    Added the Palindrome Program

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+# https://www.facebook.com/permalink.php?story_fbid=2830418890577388&id=100008279138695
+# Subscribed by Code House
+
+num=int(input("Enter a number:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is palindrome!")
+else:
+    print("Not a palindrome!")
