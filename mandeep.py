2b009bda9aa2945731857312349d36f25525e849
Mandeep kumar Gupta <57225609+Kingmandeep@users.noreply.github.com>
2020-10-01 15:08:28 +0530

    Update mandeep.py

--- mandeep.py
+++ mandeep.py
@@ -1,11 +1,22 @@
-n =int(input("Enter Number: "))
-sum=0
-t=n
-while t>0:
-   d =t%10
-   s+=d**3
-   t//=10
-if n==sum:
-  print("Armstrong Number")
- else:
-  print("Not Armstrong Number")
+# fibonacci series 
+n = int(input("Enter no. of  terms:  "))
+
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if n <= 0:
+   print("Please enter a positive integer")
+elif n == 1:
+   print("Fibonacci sequence upto",n,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < n:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
