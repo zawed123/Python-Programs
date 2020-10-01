d68edbfbea8ebf6b7957e3ac592d3cbf37e384d1
csubhasundar <72214531+csubhasundar@users.noreply.github.com>
2020-10-01 21:56:02 +0530

    Add the leap year

---
+++ Leapyear.py
@@ -0,0 +1,8 @@
+#https://m.facebook.com/story.php?story_fbid=361047445259988&id=100040642058186
+Subscribed by Code House
+
+year=int(input("Enter year : "))
+if(year%4==0 and year%100!=0 or year%400==0):
+    print("Leep year ")
+else:
+    print("Non leep year")
