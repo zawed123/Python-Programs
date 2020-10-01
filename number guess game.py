9467c40360e431a7865dcd0e56d4e86b776e4526
varun singh <58244695+armor-boop@users.noreply.github.com>
2020-10-01 17:54:22 +0530

    new loving number guess game try it and SUBSCRIBE   https://youtu.be/x084tfX4JnI
    # share https://youtu.be/x084tfX4JnI
    # subscribed by armor-boop as @varunpariharr ig handel
    #plz watch this video  https://youtu.be/x084tfX4JnI  from codehouse and subscribe for further amazing videos related to coding programming

---
+++ number guess game.py
@@ -0,0 +1,16 @@
+# share https://youtu.be/x084tfX4JnI
+# subscribed by armor-boop as @varunpariharr ig handel
+#plz watch this video  https://youtu.be/x084tfX4JnI  from codehouse and subscribe for further amazing videos related to coding programming
+
+import random
+r = random.randint(1,20)
+
+while(True):
+	inp = int(input())
+	if(inp<r):
+		print("oops,try a greater number")
+	elif(inp>r):
+		print("oops,try a smaller number")
+	else:
+		print("congrats you choosed write number")
+		break;
