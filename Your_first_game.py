5f503e98dc9ef8f12820834255c2e50832080a52
lakshyabansal303 <67966025+lakshyabansal303@users.noreply.github.com>
2020-09-30 23:03:05 -0700

    Add files via upload

---
+++ Your_first_game.py
@@ -0,0 +1,44 @@
+from random import randint
+
+#create a list of play options
+t = ["Rock", "Paper", "Scissors"]
+
+#assign a random play to the computer
+computer = t[randint(0,2)]
+
+#set player to False
+play = False
+
+while play == False:
+    print("Rock, Paper, Scissors?")
+    player = input()
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+    #player was set to True, but we want it to be False so the loop continues
+    print("Do you want to play the game again")
+    play_cont = input()
+    if play_cont == 'yes':
+        play = False
+        computer = t[randint(0,2)]
+    else:
+        print("Thanks for playing the game")  
+        break  
+
+    
