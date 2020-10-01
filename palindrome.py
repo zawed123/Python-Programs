951c6c6a7ef58eebcafcfaaacdd97a344cd9128f
Bhavesh Thale <bhaveshthale1456@gmail.com>
2020-10-01 09:36:31 +0530

    Added the Palindrome program

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+# https://www.facebook.com/bhavesh.thale.1/posts/1517693698431445
+# Subscribed by Code House
+
+def isPalindrome(s):
+    return s == s[::-1]
+ 
+s = "malayalam"
+ans = isPalindrome(s)
+ 
+if ans:
+    print("Yes")
+else:
+    print("No")
