4f196b904d68a211c20826ebae676e6ff3b9a97f
Sakshi Sharma <44578445+sakshisharma8@users.noreply.github.com>
2020-10-01 22:36:14 +0530

    send new request

---
+++ sakshi.py
@@ -0,0 +1,11 @@
+n = 20
+if n > 1:
+   for i in range(2, n//2):
+
+      if (n % i) == 0:
+         print(n, "is not a prime number")
+         break
+      else:
+         print(n, "is a prime number")
+   else:
+print(n, "is not a prime number")
